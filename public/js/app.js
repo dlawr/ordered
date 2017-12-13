863ff547d4f03e4460ea451b8adba00e3d629148
Dan <dan.lawrence3@gmail.com>
2017-12-13 10:36:31 -0500

    http request to create bookmark

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,23 @@
 const app               = angular.module('bookmarkd', []);
 
 app.controller( 'mainController' , ['$http' , function ( $http ){
+  this.form  = {};
+
   this.addBookmark = () => {
+    $http({
+      method      : 'POST',
+      url         : '/bookmarks',
+      data        : this.form
+    }).then ( response => {
+      this.bookmarks.push( response.data );
+      this.alertClass = 'alert';
+      this.alertText = 'bookmark added';
+      setTimeout ( ( ) =>{
+        this.alertClass = '';
+        this.alertText = '';
+      }, 1000);
+      this.form = {};
+    }
+  )
   }
 }]);
